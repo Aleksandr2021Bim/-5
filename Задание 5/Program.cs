0f3f9851008c2b4fc7fb4c342aee810243f0b8e6
Aleksandr Panarin <panarinalex12@gmail.com>
2021-12-06 10:07:18 +0300

    Добавьте файлы проекта.

---
+++ Задание 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7];
+            double S = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine("Введите значение");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                S += array[i];
+             }
+            double S1 = S / 7;
+            Console.WriteLine();
+            Console.WriteLine(S1);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ многомерный массив/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace многомерный_массив
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            const int m = 5;
+            int[,] array = new int[n,m];
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                   array[i, j] = (i + j) % 2;
+                   Console.Write("{0,5 } ", array[i, j]);
+                }
+               Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
